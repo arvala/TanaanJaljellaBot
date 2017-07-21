8395da2e9cda0cbe78b496bf88134f137f8d5de8
laitiar <arttu.laitinen@bcbmedical.com>
2017-07-21 14:19:05 +0300

    added tj.js, modified original example

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/*var TelegramBot = require('node-telegram-bot-api');
-var token = '383782709:AAHxEVtykKhYv9hmLaO_phPGDVlxY26k6jo';
-var bot = new TelegramBot(token, {polling: true});
-*/
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -17,18 +13,12 @@ app.post('/new-message', function(req, res) {
   const {message} = req.body
   res.send('<html><body><h1>message sent</h1></body></html>');
 
-/*bot.onText(/\/tj/, function(msg, match) {
-  var fromId = msg.from.id;
-  var morningsleft = "vitusti aamuja jäljellä";
-  bot.sendMessage(fromId, morningsleft);
-});
-*/
   //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
- // if (!message || message.text.toLowerCase().indexOf('marco') <0) {
+  if (!message || message.text.toLowerCase().indexOf('marco') <0) {
     // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
- //   return res.end()
- // }
+    return res.end()
+  }
 
   // If we've gotten this far, it means that we have received a message containing the word "marco".
   // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
@@ -49,6 +39,8 @@ app.post('/new-message', function(req, res) {
     })
 
 });
+
+
 app.get('/', function (req, res) {
 
     res.send('<html><body><h1>Flagon</h1></body></html>');
