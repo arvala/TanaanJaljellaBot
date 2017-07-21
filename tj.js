c3c9fccdc958be0a28f617b93721e8b99bdf38db
laitiar <arttu.laitinen@bcbmedical.com>
2017-07-21 14:50:10 +0300

    fix to days_between function

--- tj.js
+++ tj.js
@@ -11,12 +11,9 @@ function days_between(date1, date2) {
     // The number of milliseconds in one day
     var ONE_DAY = 1000 * 60 * 60 * 24
 
-    // Convert both dates to milliseconds
-    var date1_ms = date1.getTime()
-    var date2_ms = date2.getTime()
 
     // Calculate the difference in milliseconds
-    var difference_ms = Math.abs(date1_ms - date2_ms)
+    var difference_ms = Math.abs(date1 - date2)
 
     // Convert back to days and return
     return Math.round(difference_ms/ONE_DAY)
