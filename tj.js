2dad78c293d0658224c3084ab08e21224bab8070
Arttu Laitinen <arttu@namia.io>
2019-02-15 15:35:09 +0200

    alokas --> tykimeis

--- tj.js
+++ tj.js
@@ -72,7 +72,7 @@ bot.onText(/\/tj/, (msg) => {
 bot.onText(/\/arvo/, (msg) => {
 
   const chatId = msg.chat.id;
-  const rank = "alokas";
+  const rank = "Tykimeis";
   const surname = "Jarkko";
   const resp = "Leena tunnetaan nimellä " + rank + " " + surname;
   // send back the matched "whatever" to the chat
